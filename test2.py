acea7c53192ceadddb84009340c927606bfcbace
Asal-Emarloo <153554023+Asal-Emarloo@users.noreply.github.com>
2023-12-28 14:15:19 +0330

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,7 @@
+hours = int(input("enter hours:"))
+minutes = int(input(" enter minutes: "))
+seconds = int(input("enter seconds: "))
+
+total_seconds = hours * 3600 + minutes * 60 + seconds
+print(f"{total_seconds}")
+
